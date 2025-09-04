e4ca3c9ee9f4725dcbad4d97a3e6c9609cb4d56f
adrioui <adrifdlh@gmail.com>
2025-09-05 00:06:18 +0700

    feat(checkpoint): successfully implement checkpoint 1 and checkpoint 2
    - Mint 1000 (* 10 ** 18) to m frontend address using the constructor().
    - Implement `buyTokens` function

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.20; //Do not change the solidity version as it negatively im
 import "./YourToken.sol";
 
 contract Vendor {
-    // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
     YourToken public yourToken;
 
@@ -13,7 +13,21 @@ contract Vendor {
         yourToken = YourToken(tokenAddress);
     }
 
+    uint256 public constant tokensPerEth = 100;
+
     // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+      require(msg.value > 0, "Send ETH to buy tokens");
+
+      // Calculate tokens to buy
+      uint256 tokensToBuy = (msg.value * tokensPerEth * (10 ** 18)) / 1 ether;
+      require(yourToken.balanceOf(address(this)) >= tokensToBuy, "Vendor: insufficient token balance (ERC20InsufficientBalance)");
+
+      bool sent = yourToken.transfer(msg.sender, tokensToBuy);
+      require(sent, "Token transfer failed");
+
+      emit BuyTokens(msg.sender, msg.value, tokensToBuy);
+    }
 
     // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
